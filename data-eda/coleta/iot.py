cac4679c1f1b413bb56bb2a35ac99ba8c3674cac
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-10 12:42:02 -0300

    chore(global): estrutura padrão do projeto

---
+++ data-eda/coleta/iot.py
@@ -0,0 +1,21 @@
+# módulo responsavel por coletar dados da api do iot e transformar em um dataframe pandas
+
+import requests
+import pandas as pd
+
+def coletar_dados_iot():
+    url = "https://minimeteorolia-1.onrender.com/medicoes"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        dados_json = response.json()
+        df = pd.DataFrame(dados_json)
+        return df
+    else:
+        print(f"Erro ao coletar dados da API: {response.status_code}")
+        return pd.DataFrame()  
+
+
+def salvar_dados_csv(df, path, sep=';', encoding='utf-8'):
+    df.to_csv(path, index=True, sep=sep, encoding=encoding)
+    print(f"Dados salvos em {path}")
