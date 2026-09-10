0ec123e55151004925b647b233b85ca4c43caee5
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-10 13:00:26 -0300

    feat(coleta): agora podemos salvar dados brutos do iot no banco do projeto django

--- data-eda/coleta/iot.py
+++ data-eda/coleta/iot.py
@@ -1,6 +1,7 @@
 # módulo responsavel por coletar dados da api do iot e transformar em um dataframe pandas
 
 import requests
+import sqlite3
 import pandas as pd
 
 def coletar_dados_iot():
@@ -19,4 +20,11 @@ def coletar_dados_iot():
 def salvar_dados_csv(df, path, sep=';', encoding='utf-8'):
     df.to_csv(path, index=True, sep=sep, encoding=encoding)
     print(f"Dados salvos em {path}")
+    
+def salvar_dados_sql(df, db_path, table_name):
+    import sqlite3
+    path = sqlite3.connect(db_path)
+    df.to_sql(table_name, path, if_exists='replace', index=True)
+    path.close()
+    print(f"Dados salvos na tabela '{table_name}' do banco de dados '{db_path}'")
 
