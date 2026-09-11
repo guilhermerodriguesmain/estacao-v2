e75808f910ceacb8068df6297901b021555a8b25
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-10 21:58:40 -0300

    fix(analise): corrige modificação de dados não esperada no metodo frequencia_medicoes()

--- data-eda/analise/eda.py
+++ data-eda/analise/eda.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 class EDA:
-    def __init__(self, df):
+    def __init__(self, df: pd.DataFrame):
         self.df = df
 
     # Retorna um resumo estatístico do dataframe
@@ -22,63 +22,66 @@ class EDA:
         return self.df.dtypes
 
     # retorna periodos de tempo analisado com base na coluna de data
-    def periodo_tempo(self, coluna_data):
-        if coluna_data in self.df.columns:
-            data_min = self.df[coluna_data].min()
-            data_max = self.df[coluna_data].max()
+    def periodo_tempo(self, coluna_dados):
+        if coluna_dados in self.df.columns:
+            data = self.df.copy()
+            data[coluna_dados] = pd.to_datetime(data[coluna_dados])
+            data_min = data[coluna_dados].min()
+            data_max = data[coluna_dados].max()
             return data_min, data_max
         else:
-            raise ValueError(f"A coluna '{coluna_data}' não existe no dataframe.")
+            raise ValueError(f"A coluna '{coluna_dados}' não existe no dataframe.")
 
     # Retorna frequencia das medicoes por hora, dia, semana, mes e ano com base na coluna de data
-    def frequencia_medicoes(self, coluna_data):
-        if coluna_data in self.df.columns:
-            self.df[coluna_data] = pd.to_datetime(self.df[coluna_data])
+    def frequencia_medicoes(self, coluna_dados):
+        data = self.df.copy()
+        if coluna_dados in data.columns:
+            data[coluna_dados] = pd.to_datetime(data[coluna_dados])
             frequencia = {
-                "hora": self.df.set_index(coluna_data).resample('H').size(),
-                "dia": self.df.set_index(coluna_data).resample('D').size(),
-                "semana": self.df.set_index(coluna_data).resample('W').size(),
-                "mes": self.df.set_index(coluna_data).resample('M').size(),
-                "ano": self.df.set_index(coluna_data).resample('Y').size()
+                "hora": data.set_index(coluna_dados).resample('h').size(),
+                "dia": data.set_index(coluna_dados).resample('D').size(),
+                "semana": data.set_index(coluna_dados).resample('W').size(),
+                "mes": data.set_index(coluna_dados).resample('ME').size(),
+                "ano": data.set_index(coluna_dados).resample('YE').size()
             }
             return frequencia
         else:
-            raise ValueError(f"A coluna '{coluna_data}' não existe no dataframe.")
+            raise ValueError(f"A coluna '{coluna_dados}' não existe no dataframe.")
     
     # Retorna variaveis disponiveis no dataframe
     def variaveis_disponiveis(self):
         return self.df.columns.tolist()
 
     # Retorna valores maximos e minimos de cada variavel do dataframe
-    def valores_maximos_minimos(self, column_data):
-        if column_data in self.df.columns:
-            return self.df[column_data].min(), self.df[column_data].max()
+    def valores_maximos_minimos(self, column_dados):
+        if column_dados in self.df.columns:
+            return self.df[column_dados].min(), self.df[column_dados].max()
         else:
-            raise ValueError(f"A coluna '{column_data}' não existe no dataframe.")
+            raise ValueError(f"A coluna '{column_dados}' não existe no dataframe.")
     
     # Retorna a quantidade de registros no dataframe
     def quantidade_registros(self):
         return len(self.df)
 
     # Retorna media, mediana e desvio padrao de cada variavel do dataframe
-    def estatisticas_descritivas(self, column_data):
-        if column_data in self.df.columns:
+    def estatisticas_descritivas(self, column_dados):
+        if column_dados in self.df.columns:
             return {
-                "media": self.df[column_data].mean(),
-                "mediana": self.df[column_data].median(),
-                "desvio_padrao": self.df[column_data].std()
+                "media": self.df[column_dados].mean(),
+                "mediana": self.df[column_dados].median(),
+                "desvio_padrao": self.df[column_dados].std()
             }
         else:
-            raise ValueError(f"A coluna '{column_data}' não existe no dataframe.")
+            raise ValueError(f"A coluna '{column_dados}' não existe no dataframe.")
 
     # Retorna possiveis anomalias ou outliers em cada variavel do dataframe usando o metodo do IQR (Interquartile Range)
-    def detectar_anomalias_outliers(self, column_data):
-        if column_data in self.df.columns:
-            Q1 = self.df[column_data].quantile(0.25)
-            Q3 = self.df[column_data].quantile(0.75)
+    def detectar_anomalias_outliers(self, column_dados):
+        if column_dados in self.df.columns:
+            Q1 = self.df[column_dados].quantile(0.25)
+            Q3 = self.df[column_dados].quantile(0.75)
             IQR = Q3 - Q1
-            outliers = self.df[(self.df[column_data] < (Q1 - 1.5 * IQR)) | (self.df[column_data] > (Q3 + 1.5 * IQR))]
+            outliers = self.df[(self.df[column_dados] < (Q1 - 1.5 * IQR)) | (self.df[column_dados] > (Q3 + 1.5 * IQR))]
             return outliers
         else:
-            raise ValueError(f"A coluna '{column_data}' não existe no dataframe.")
+            raise ValueError(f"A coluna '{column_dados}' não existe no dataframe.")
     
