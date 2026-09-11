dfb4d6e49481bfbf48e79c0337a9db7501d280a3
Guilherme Rodrigues <guilh@Guilherme.localdomain>
2026-09-11 01:25:45 -0300

    fix(processamento): corrige erro de tratamento e normalização dos dados para que pudessem ser processados corretamente

--- data-eda/processamento/processamento.py
+++ data-eda/processamento/processamento.py
@@ -1,4 +1,5 @@
 ## define classe de processamento de dados do openmeteo e IoT
+import pandas as pd
 
 class Processamento():
     
@@ -14,22 +15,37 @@ class Processamento():
             'rain': 'chuva',
             'pressure_msl': 'pressao'
         })
+        df['data'] = pd.to_datetime(df['data'], utc=True)
         return df
 
     def padronizar_dataframe_iot(self, df_iot):
         # Padroniza o dataframe para ter colunas consistentes
-        df = df.rename(columns={
+        df = df_iot.rename(columns={
             'temperatura': 'temperatura',
             'umidade': 'umidade_relativa',
             'timestamp': 'data'
         })
-
+        df['data'] = pd.to_datetime(df['data'], utc=True)
 
         return df
 
     def mesclar_dataframes(self, df_iot, df_openmeteo):
         # Mescla os dataframes do IoT e do OpenMeteo com base na coluna de data
-        df_merged = pd.merge(df_iot, df_openmeteo, on='date', how='outer')
+        df_iot = df_iot.sort_values('data')
+        df_iot = df_iot.drop(columns=['fonte'], errors='ignore')
+
+        df_openmeteo = df_openmeteo.sort_values('data')
+        df_openmeteo = df_openmeteo.drop(columns=['fonte'], errors='ignore')
+        df_openmeteo = df_openmeteo.drop(columns=['Unnamed: 0'],errors='ignore')
+
+        df_merged = pd.merge_asof(
+            df_iot, 
+            df_openmeteo, 
+            on='data', 
+            direction='nearest',
+            tolerance=pd.Timedelta('30min'),
+            suffixes=('_iot', '_openmeteo')
+            )
         return df_merged
 
     def salvar_dados_csv(self, df, path, sep=';', encoding='utf-8'):
@@ -43,3 +59,22 @@ class Processamento():
         path.close()
         print(f"Dados salvos na tabela '{table_name}' do banco de dados '{db_path}'")
         
+padronizar = Processamento()
+
+dados_iot_padronizados = padronizar.padronizar_dataframe_iot(
+    pd.read_csv(
+        "../dados/dados_iot.csv", 
+        sep=';', 
+        encoding='utf-8',
+        usecols=['temperatura', 'umidade', 'timestamp']) )
+
+dados_openmeteo_padronizados = padronizar.padronizar_dataframe_openmeteo(
+    pd.read_csv(
+        "../dados/dados_openmeteo.csv", 
+        sep=';', 
+        encoding='utf-8',
+        ) )
+
+dados_mesclados = padronizar.mesclar_dataframes(dados_iot_padronizados, dados_openmeteo_padronizados)
+
+padronizar.salvar_dados_csv(dados_mesclados, "../dados/dados_meteorologicos.csv")
